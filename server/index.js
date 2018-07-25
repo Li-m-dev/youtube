dfe6b70c294a539549a6f3e6d18c289f67a20421
Li Swafford <liswafford@yahoo.com>
2018-07-25 16:07:20 -0500

    add

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const port = 8081;
 const app = express();
+app.use(express.static(`${__dirname}/../build`));
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
